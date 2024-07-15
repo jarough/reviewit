a0ddd4da9ffe84e0e8329b537ea206b4af60907d
jarough <jackroughead@gmail.com>
2024-07-15 11:20:34 +0100

    Created 'login.html' template with a working login system that checks inputs against those registered in the database

--- app.py
+++ app.py
@@ -25,7 +25,8 @@ def get_books():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
-        existing_user = mongo.users.find_one({"username": request.form.get("username").lower()})
+        existing_user = mongo.users.find_one(
+            {"username": request.form.get("username").lower()})
 
         if existing_user:
             flash("Username already taken")
@@ -37,10 +38,34 @@ def register():
         }
         mongo.users.insert_one(register)
 
-        # new user session
+        # New user session
         session["user"] = request.form.get("username").lower()
         flash("Register was successful!")
     return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # Checks if username exists
+        existing_user = mongo.users.find_one(
+            {"username": request.form.get("username").lower()})
+        # Checks if password matches
+        if existing_user:
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome {}".format(request.form.get("username")))
+            else:
+                flash("Incorrect User/Password")
+                return redirect(url_for("login"))
+            
+        else:
+            flash("Incorrect User/Password")
+            return redirect(url_for("login"))
+
+
+    return render_template("login.html")
 
 
 if __name__ == "__main__":
